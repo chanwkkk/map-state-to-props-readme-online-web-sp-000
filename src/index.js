5bea6a96e4343e97495b1260816159c520127ef2
Yingqi Chen <chanwk18@hotmail.com>
2020-02-26 04:55:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-  <App />
-</Provider>,
+   <Provider store={store}>
+     <App />
+   </Provider>,
   document.getElementById('root')
 );
